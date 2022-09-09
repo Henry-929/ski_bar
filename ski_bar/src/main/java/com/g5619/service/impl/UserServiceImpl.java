ac171717be21c5c1be621e622baca00d34901e70
BRUCERUIZE <96535703+BRUCERUIZE@users.noreply.github.com>
2022-09-09 23:57:05 +0800

    Add files via upload

--- ski_bar/src/main/java/com/g5619/service/impl/UserServiceImpl.java
+++ ski_bar/src/main/java/com/g5619/service/impl/UserServiceImpl.java
@@ -1,10 +1,15 @@
 package com.g5619.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.g5619.entity.User;
 import com.g5619.mapper.UserMapper;
 import com.g5619.service.UserService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +22,23 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
 
+    @Autowired
+    UserMapper userMapper;
+
+    /**
+     * 登录
+     */
+    @Override
+    public User login(String username, String password, String type) {
+        HashMap<String, Object> map = new HashMap<>();
+        // 自定义要查询
+        map.put(username,username);
+        map.put(password,password);
+        map.put(type,type);
+        List<User> users = userMapper.selectByMap(map);
+        if (users.size()>0){
+            return users.get(0);
+        }
+        return null;
+    }
 }
