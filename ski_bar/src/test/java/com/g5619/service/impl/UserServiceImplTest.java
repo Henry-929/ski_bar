a41df8ef26ac62245711d26290fe56605055f350
yiweixu <511878701@qq.com>
2022-10-11 17:44:44 +0800

    修改

--- ski_bar/src/test/java/com/g5619/service/impl/UserServiceImplTest.java
+++ ski_bar/src/test/java/com/g5619/service/impl/UserServiceImplTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import sun.jvm.hotspot.utilities.Assert;
+
 
 @SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.RANDOM_PORT)
 class UserServiceImplTest {
